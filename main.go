6e7157cf0aabf7874398874bef1173fd2332501c
jdxgfty <62816854+jdxgfty@users.noreply.github.com>
2022-06-07 22:28:04 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		img, err := os.ReadFile("stop_horny.jpg")
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("an unknown error occured"))
+			return
+		}
+		w.Header().Set("Content-Disposition", `inline; filename="stop_horny.jpg"`)
+		w.Header().Set("Content-Length", fmt.Sprint(len(img)))
+		w.Write(img)
+	})
+
+	log.Fatalln(http.ListenAndServe(":8080", mux))
+}
