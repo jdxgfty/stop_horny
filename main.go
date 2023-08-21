41399d8ca29c7d5410517a582f23733c497ef7d5
jdxgfty <62816854+jdxgfty@users.noreply.github.com>
2023-08-21 23:16:04 +0300

    Use go embed

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"embed"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 var ALLOWED_HEADERS = []string{"GET", "HEAD"}
+
+//go:embed stop_horny.jpg
+var imgFS embed.FS
 
 func main() {
 	e := echo.New()
@@ -16,10 +22,19 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.Match(ALLOWED_HEADERS, "/*.", func(c echo.Context) error {
-		return c.Inline("stop_horny.jpg", "stop_horny.jpg")
+		return inline(c, "stop_horny.jpg", "stop_horny.jpg")
 	})
 
 	log.Println("Starting server")
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Error(e.Start(":8080"))
+}
+
+func inline(c echo.Context, file, name string) error {
+	return contentDisposition(c, file, name, "inline")
+}
+
+func contentDisposition(c echo.Context, file, name, dispositionType string) error {
+	c.Response().Header().Set(echo.HeaderContentDisposition, fmt.Sprintf("%s; filename=%q", dispositionType, name))
+	return c.FileFS(name, imgFS)
 }
